7bf020881466ec68ac2ec64da79f0049ac61e882
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-18 12:06:52 +0700

    Use email from env config.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = process.env.EMAIL || "quanglng2022@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
